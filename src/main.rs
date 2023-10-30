93a2773e5d47c42d1e5a0e900f1617f380bbeff0
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2023-10-30 14:20:59 +0000

    feat: update main fn

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io;
+use std::io::Read;
+
+use notation::problem::Problem;
+
+mod notation;
+mod parser;
+
+use parser::cnfparser;
+
 fn main() {
-    println!("SAT Solver written in Rust");
+    // SAT Solver written in Rust
+    // Usage: cat problem.cnf | ./sat
+
+    // Read from stdin
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer).unwrap();
+
+    // Print the contents of the file
+    println!("{}", buffer);
+
+    let _prob: Problem = cnfparser::parse_cnf(&buffer);
+
+    // Parse the problem
+    // let _problem = parse(&problem);
 }
